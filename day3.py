82fb64b9402abef67d87bf46b6aa77357061d1be
Ayush Mishra <officialayushmishra002m>
2026-01-31 23:16:02 +0530

    learning class in python

---
+++ day3.py
@@ -0,0 +1,66 @@
+class person():
+    name = "Ayush Mishra"
+    occupation = "ai engineer"
+    sallary = 400
+
+    def info(self):
+        print(f"{self.name} is a {self.occupation}")
+
+a = person()
+# a.name = "Harsh Mishra"
+a.occupation = "AI Engineer"
+
+
+c = person()
+c.name = "Vaibhav"
+c.occupation = "Game Developer"
+b = person()
+b.name = "Shubhm"
+b.occupation = "Tax Collector"
+
+
+a.info()
+c.info()
+b.info()
+
+
+class person():
+
+    def __init__(self, name, occupation):
+        self.name = name
+        self.occupation = occupation
+
+    def info(self):
+        print(f"{self.name} is an {self.occupation} in this firm")
+
+a = person("Ayush Mishra", "AI Engineer")
+a.info()        
+
+# Decorator 
+
+def greet(fx):
+    def mfx(*args, **kwargs):
+        print("Jai Hind")
+        fx(*args, **kwargs)
+        print("Thanks for using this function")
+    return mfx
+
+
+@greet
+def add(a,b):
+    sum = a+b
+    print("The addition of two number is :", sum )
+add(6, 7)
+
+
+# ---------random function---------- #
+# @greet
+# def hello():
+#     print("hello someone born with glorious perpose")
+# hello()
+
+
+
+
+
+
